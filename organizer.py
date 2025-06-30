11a4548162414590048df30d339f08955d6e78d7
mrg-reen <eukpong06@gmail.com>
2025-06-29 20:45:52 -0400

    organizer tool; initial commit

---
+++ organizer.py
@@ -0,0 +1,48 @@
+import os
+import re
+import shutil
+
+
+class Organizer:
+    def __init__(self, directory, filetype):
+        self.directory = directory
+        self.filetype = filetype
+
+    #need a function to first compile or gather all of the files of the same type
+    # https://docs.python.org/3/library/os.html
+    # https://www.programiz.com/python-programming/directory
+
+    #use os.listdir() to read directory
+
+    def compile_files(self):
+        os.chdir(self.directory)
+
+        cleaned_filetype = re.sub(r'[^a-zA-Z0-9]', '', self.filetype)
+
+        directory_files = os.listdir()
+        
+        try:
+            if not f'{cleaned_filetype}_folder' in self.directory:
+                os.mkdir(f'{cleaned_filetype}_folder')
+
+            self.organize(directory_files, cleaned_filetype)
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def organize(self, directory_files, cleaned_filetype):
+        try:
+            for file in directory_files:
+                if file.endswith(f'{self.filetype}'):
+                    shutil.move(file, f'{cleaned_filetype}_folder')
+                    print(f'{file} moved to {cleaned_filetype}_folder')
+        except FileNotFoundError:
+            print(f"Error: Directory '{directory_files}' not found.")
+        except Exception as e:
+            print(f'An error occured: {e}')
+        
+
+if __name__ == "__main__":
+    directory_m = input(f'Input the full directory path you want to target: ')
+    filetype_m = input(f'Input the filetype to target: ')
+    organizer = Organizer(directory_m, filetype_m)
+    organizer.compile_files()
